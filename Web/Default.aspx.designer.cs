38751037d022ef699d2bbbb66a26b3be1ffe72e2
Pablo Pelardas <pablo.pelardas@gmail.com>
2023-10-06 10:02:57 -0300

    Revert "test"
    This reverts commit 9626a098d456f86daa8e817ef716ac609392d4c5.

--- Web/Default.aspx.designer.cs
+++ Web/Default.aspx.designer.cs
@@ -13,5 +13,23 @@ namespace Web
 
     public partial class _Default
     {
+
+        /// <summary>
+        /// ddlMarca control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlMarca;
+
+        /// <summary>
+        /// ddlCategoria control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCategoria;
     }
 }
