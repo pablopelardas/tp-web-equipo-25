553bc59ff4a0896059b40ed1188ece514aaf22a2
braian-valor <braian.valor@alumnos.frgp.utn.edu.ar>
2023-10-05 21:41:34 -0300

    lectura de articulos y vista en cards

--- Web/Default.aspx
+++ Web/Default.aspx
@@ -7,9 +7,35 @@
         <div class="col-6">
             <asp:DropDownList ID="ddlMarca" CssClass="form-select mx-auto w-auto" runat="server"></asp:DropDownList>
         </div>
-        <div class="col-6">         
+        <div class="col-6">
             <asp:DropDownList ID="ddlCategoria" CssClass="form-select mx-auto w-auto" runat="server"></asp:DropDownList>
         </div>
+
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+
+        <%
+            foreach (Dominio.Articulo item in ListaArticulos)
+            {
+        %>
+                <div class="col">
+                    <div class="card">
+                        <%foreach (Dominio.Imagen i in ListaImagenes)
+                            {
+                                if (i.IdArticulo == item.Id)
+                                {
+                                    ImgSeleccionada = i.URL;
+                                }
+                            } %>
+                        <img src="<%: ImgSeleccionada %>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><%: item.Nombre %></h5>
+                            <p class="card-text"><%: item.Descripcion %></p>
+                        </div>
+                    </div>
+                </div>
+        <%  } %>
 
     </div>
 
--- Web/Default.aspx.cs
+++ Web/Default.aspx.cs
@@ -1,4 +1,5 @@
-﻿using Negocio;
+﻿using Dominio;
+using Negocio;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,10 @@ namespace Web
 {
     public partial class _Default : Page
     {
+        public List<Articulo> ListaArticulos { get; set; }
+        public List<Imagen> ListaImagenes { get; set; }
+        public string ImgSeleccionada { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["listaMarcas"] == null)
@@ -22,6 +27,19 @@ namespace Web
                 CategoriaNegocio categoriaNegocio = new CategoriaNegocio();
                 Session.Add("listaCategorias", categoriaNegocio.ListarCategoriaConSP());
             }
+            if (Session["listaArticulos"] == null)
+            {
+                ArticulosNegocio articuloNegocio = new ArticulosNegocio();
+                Session.Add("listaArticulos", articuloNegocio.ListarArticulosConSP());
+            }
+            if (Session["listaImagenes"] == null)
+            {
+                ImagenNegocio imgNegocio = new ImagenNegocio();
+                Session.Add("listaImagenes", imgNegocio.ListarImagenesConSP());
+            }
+
+            ListaArticulos = (List<Articulo>)Session["listaArticulos"];
+            ListaImagenes = (List<Imagen>)Session["listaImagenes"];
 
             ddlMarca.DataSource = Session["listaMarcas"];
             ddlMarca.DataBind();
